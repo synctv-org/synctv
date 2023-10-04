8138da0aca48ebbd74d4e2ebf981ff433a0c4c75
zijiren233 <pyh1670605849@gmail.com>
2023-10-04 22:51:37 +0800

    Feat: build wordflow and auto dev

--- cmd/version.go
+++ cmd/version.go
@@ -2,8 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/spf13/cobra"
+	"github.com/synctv-org/synctv/internal/conf"
 )
 
 var VersionCmd = &cobra.Command{
@@ -11,7 +13,12 @@ var VersionCmd = &cobra.Command{
 	Short: "Print the version number of Sync TV Server",
 	Long:  `All software has versions. This is Sync TV Server's`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("synctv-server v0.1 -- HEAD")
+		fmt.Printf("Sync TV Server version %s\n", conf.Version)
+		fmt.Printf("Sync TV Web version %s\n", conf.WebVersion)
+		fmt.Printf("Git commit %s\n", conf.GitCommit)
+		fmt.Printf("Go version %s\n", runtime.Version())
+		fmt.Printf("Built with %s\n", runtime.Compiler)
+		fmt.Printf("OS/Arch %s/%s\n", runtime.GOOS, runtime.GOARCH)
 	},
 }
 

b3658513dfe76a69fc2710c4a96d4baa5e477aee
A1KESH1 <xziye666@gmail.com>
2023-11-20 17:36:26 +0800

    添加注释：FeishuSSO Plugin

--- internal/provider/plugins/example/example_feishuSSO/example_feishuSSO.go
+++ internal/provider/plugins/example/example_feishuSSO/example_feishuSSO.go
@@ -20,27 +20,30 @@ import (
 //
 // mv gitee {data-dir}/plugins/oauth2/feishuSSO
 //
+// 飞书SSO登录插件设置信息：飞书集成平台-身份集成-应用单点登录-你创建的SSO服务
+//
 // config.yaml:
 //
 //	oauth2:
 //		providers:
 //			feishuSSO:
-//				client_id: xxx
-//				client_secret: xxx
-//				redirect_url: xxx
+//				client_id: "App ID"
+//				client_secret: "App Secret"
+//				redirect_url: "登录回调地址"
 //		plugins:
 //			- plugin_file: plugins/oauth2/feishuSSO
-//			  arges: "Your SSO Application ID in Feishu Anycross"
+//			  arges:
+//				- "OAuth2.0协议端点中的一串数字"
 type FeishuProvider struct {
 	config oauth2.Config
-	ssoid  string
+	ssoid  string // Your SSO Application ID in Feishu Anycross
 }
 
 func (p *FeishuProvider) Init(c provider.Oauth2Option) {
 	p.config.Scopes = []string{"profile"}
 	p.config.Endpoint = oauth2.Endpoint{
-		AuthURL:  fmt.Sprintf("https://anycross.feishu.cn/sso/%s/oauth2/auth", p.ssoid),
-		TokenURL: fmt.Sprintf("https://anycross.feishu.cn/sso/%s/oauth2/token", p.ssoid),
+		AuthURL:  fmt.Sprintf("https://anycross.feishu.cn/sso/%s/oauth2/auth", p.ssoid),  // 认证端点
+		TokenURL: fmt.Sprintf("https://anycross.feishu.cn/sso/%s/oauth2/token", p.ssoid), //Token 端点
 	}
 	p.config.ClientID = c.ClientID
 	p.config.ClientSecret = c.ClientSecret
@@ -65,7 +68,7 @@ func (p *FeishuProvider) RefreshToken(ctx context.Context, tk string) (*oauth2.T
 
 func (p *FeishuProvider) GetUserInfo(ctx context.Context, tk *oauth2.Token) (*provider.UserInfo, error) {
 	client := p.config.Client(ctx, tk)
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("https://anycross.feishu.cn/sso/%s/oauth2/userinfo", p.ssoid), nil)
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("https://anycross.feishu.cn/sso/%s/oauth2/userinfo", p.ssoid), nil) // 身份端点
 	if err != nil {
 		return nil, err
 	}
@@ -86,8 +89,8 @@ func (p *FeishuProvider) GetUserInfo(ctx context.Context, tk *oauth2.Token) (*pr
 }
 
 type feishuUserInfo struct {
-	UserID string `json:"user_id"`
-	Name   string `json:"name"`
+	UserID string `json:"user_id"` // 飞书UserID
+	Name   string `json:"name"`    // 飞书姓名
 }
 
 func main() {

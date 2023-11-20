c567326f2ece9c8f722eef4a405bf6a9f40ff7ab
A1KESH1 <xziye666@gmail.com>
2023-11-20 17:19:10 +0800

    添加新登录插件：飞书SSO(增加通过args读取ssoid)

--- internal/provider/plugins/example/example_feishuSSO/example_feishuSSO.go
+++ internal/provider/plugins/example/example_feishuSSO/example_feishuSSO.go
@@ -3,28 +3,48 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"net/http"
-
+	"fmt"
 	plugin "github.com/hashicorp/go-plugin"
 	"github.com/synctv-org/synctv/internal/provider"
 	"github.com/synctv-org/synctv/internal/provider/plugins"
 	"golang.org/x/oauth2"
+	"net/http"
+	"os"
 )
 
+// Mac/Linux:
+// go build -o feishuSSO ./internal/provider/plugins/example/example_feishuSSO/example_feishuSSO.go
+//
+// Windows:
+// go build -o feishuSSO.exe ./internal/provider/plugins/example/example_feishuSSO/example_feishuSSO.go
+//
+// mv gitee {data-dir}/plugins/oauth2/feishuSSO
+//
+// config.yaml:
+//
+//	oauth2:
+//		providers:
+//			feishuSSO:
+//				client_id: xxx
+//				client_secret: xxx
+//				redirect_url: xxx
+//		plugins:
+//			- plugin_file: plugins/oauth2/feishuSSO
+//			  arges: "Your SSO Application ID in Feishu Anycross"
 type FeishuProvider struct {
 	config oauth2.Config
+	ssoid  string
 }
 
 func (p *FeishuProvider) Init(c provider.Oauth2Option) {
 	p.config.Scopes = []string{"profile"}
 	p.config.Endpoint = oauth2.Endpoint{
-		AuthURL:  "飞书集成平台-身份集成-oauth2授权端点",
-		TokenURL: "飞书集成平台-身份集成-oauth2Token端点",
+		AuthURL:  fmt.Sprintf("https://anycross.feishu.cn/sso/%s/oauth2/auth", p.ssoid),
+		TokenURL: fmt.Sprintf("https://anycross.feishu.cn/sso/%s/oauth2/token", p.ssoid),
 	}
 	p.config.ClientID = c.ClientID
 	p.config.ClientSecret = c.ClientSecret
 	p.config.RedirectURL = c.RedirectURL
-	//请修改 GetUserInfo() 中的URL
 }
 
 func (p *FeishuProvider) Provider() provider.OAuth2Provider {
@@ -45,7 +65,7 @@ func (p *FeishuProvider) RefreshToken(ctx context.Context, tk string) (*oauth2.T
 
 func (p *FeishuProvider) GetUserInfo(ctx context.Context, tk *oauth2.Token) (*provider.UserInfo, error) {
 	client := p.config.Client(ctx, tk)
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "飞书集成平台-身份集成-oauth2用户信息端点", nil)
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("https://anycross.feishu.cn/sso/%s/oauth2/userinfo", p.ssoid), nil)
 	if err != nil {
 		return nil, err
 	}
@@ -71,8 +91,9 @@ type feishuUserInfo struct {
 }
 
 func main() {
+	args := os.Args
 	var pluginMap = map[string]plugin.Plugin{
-		"Provider": &plugins.ProviderPlugin{Impl: &FeishuProvider{}},
+		"Provider": &plugins.ProviderPlugin{Impl: &FeishuProvider{ssoid: args[1]}},
 	}
 	plugin.Serve(&plugin.ServeConfig{
 		HandshakeConfig: plugins.HandshakeConfig,

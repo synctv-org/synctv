22d94b975edafbb5f3ecec9bb7c4bb34d1a77ddf
A1KESH1 <xziye666@gmail.com>
2023-11-20 16:03:09 +0800

    添加新登录插件：飞书SSO

--- internal/provider/plugins/example/feishuSSO.go
+++ internal/provider/plugins/example/feishuSSO.go
@@ -1,33 +1,34 @@
-package providers
+package main
 
 import (
 	"context"
-	"fmt"
-	json "github.com/json-iterator/go"
-	"github.com/synctv-org/synctv/internal/provider"
-	"golang.org/x/oauth2"
+	"encoding/json"
 	"net/http"
+
+	plugin "github.com/hashicorp/go-plugin"
+	"github.com/synctv-org/synctv/internal/provider"
+	"github.com/synctv-org/synctv/internal/provider/plugins"
+	"golang.org/x/oauth2"
 )
 
 type FeishuProvider struct {
 	config oauth2.Config
-	SSOID  string
 }
 
 func (p *FeishuProvider) Init(c provider.Oauth2Option) {
-	var SSOID = c.FeishuSSOID
 	p.config.Scopes = []string{"profile"}
 	p.config.Endpoint = oauth2.Endpoint{
-		AuthURL:  fmt.Sprintf("https://anycross.feishu.cn/sso/%s/oauth2/auth", SSOID),
-		TokenURL: fmt.Sprintf("https://anycross.feishu.cn/sso/571495907/oauth2/token", SSOID),
+		AuthURL:  "飞书集成平台-身份集成-oauth2授权端点",
+		TokenURL: "飞书集成平台-身份集成-oauth2Token端点",
 	}
 	p.config.ClientID = c.ClientID
 	p.config.ClientSecret = c.ClientSecret
 	p.config.RedirectURL = c.RedirectURL
+	//请修改 GetUserInfo() 中的URL
 }
 
 func (p *FeishuProvider) Provider() provider.OAuth2Provider {
-	return "feishu"
+	return "feishuSSO"
 }
 
 func (p *FeishuProvider) NewAuthURL(state string) string {
@@ -44,7 +45,7 @@ func (p *FeishuProvider) RefreshToken(ctx context.Context, tk string) (*oauth2.T
 
 func (p *FeishuProvider) GetUserInfo(ctx context.Context, tk *oauth2.Token) (*provider.UserInfo, error) {
 	client := p.config.Client(ctx, tk)
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("https://anycross.feishu.cn/sso/%s/oauth2/userinfo", p.SSOID), nil)
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "飞书集成平台-身份集成-oauth2用户信息端点", nil)
 	if err != nil {
 		return nil, err
 	}
@@ -69,6 +70,13 @@ type feishuUserInfo struct {
 	Name   string `json:"name"`
 }
 
-func init() {
-	RegisterProvider(new(FeishuProvider))
+func main() {
+	var pluginMap = map[string]plugin.Plugin{
+		"Provider": &plugins.ProviderPlugin{Impl: &FeishuProvider{}},
+	}
+	plugin.Serve(&plugin.ServeConfig{
+		HandshakeConfig: plugins.HandshakeConfig,
+		Plugins:         pluginMap,
+		GRPCServer:      plugin.DefaultGRPCServer,
+	})
 }

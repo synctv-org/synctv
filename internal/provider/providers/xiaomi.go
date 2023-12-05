781428ca215fd9931e20507b10651ef0562e6d4c
A1KESH1 <xziye666@gmail.com>
2023-12-05 16:25:51 +0800

    新增：内置小米登录

---
+++ internal/provider/providers/xiaomi.go
@@ -0,0 +1,83 @@
+package providers
+
+import (
+	"context"
+	"fmt"
+	json "github.com/json-iterator/go"
+	"github.com/synctv-org/synctv/internal/provider"
+	"golang.org/x/oauth2"
+	"net/http"
+)
+
+type XiaomiProvider struct {
+	config oauth2.Config
+}
+
+func newXiaomiProvider() provider.ProviderInterface {
+	return &XiaomiProvider{
+		config: oauth2.Config{
+			Scopes: []string{"profile"},
+			Endpoint: oauth2.Endpoint{
+				AuthURL:  "https://account.xiaomi.com/oauth2/authorize",
+				TokenURL: "https://account.xiaomi.com/oauth2/token",
+			},
+		},
+	}
+}
+
+func (p *XiaomiProvider) Init(c provider.Oauth2Option) {
+	p.config.ClientID = c.ClientID
+	p.config.ClientSecret = c.ClientSecret
+	p.config.RedirectURL = c.RedirectURL
+}
+
+func (p *XiaomiProvider) Provider() provider.OAuth2Provider {
+	return "xiaomi"
+}
+
+func (p *XiaomiProvider) NewAuthURL(state string) string {
+	return p.config.AuthCodeURL(state, oauth2.AccessTypeOnline)
+}
+
+func (p *XiaomiProvider) GetToken(ctx context.Context, code string) (*oauth2.Token, error) {
+	return p.config.Exchange(ctx, code)
+}
+
+func (p *XiaomiProvider) RefreshToken(ctx context.Context, tk string) (*oauth2.Token, error) {
+	return p.config.TokenSource(ctx, &oauth2.Token{RefreshToken: tk}).Token()
+}
+
+func (p *XiaomiProvider) GetUserInfo(ctx context.Context, tk *oauth2.Token) (*provider.UserInfo, error) {
+	client := p.config.Client(ctx, tk)
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("https://open.account.xiaomi.com/user/profile?clientId=%s&token=%s", p.config.ClientID, tk.AccessToken), nil)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	ui := ResponseData{}
+	err = json.NewDecoder(resp.Body).Decode(&ui)
+	if err != nil {
+		return nil, err
+	}
+	return &provider.UserInfo{
+		Username:       ui.Data.Name,
+		ProviderUserID: ui.Data.UnionId,
+	}, nil
+}
+
+type ResponseData struct {
+	Data XiaomiUserInfo `json:"data"`
+}
+
+type XiaomiUserInfo struct {
+	UnionId string `json:"unionId"`
+	Name    string `json:"miliaoNick"`
+}
+
+func init() {
+	RegisterProvider(newXiaomiProvider())
+}

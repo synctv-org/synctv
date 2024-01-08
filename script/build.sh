88ee1a6475f11fd6c64210145a4947b60b044cc6
zijiren233 <pyh1670605849@gmail.com>
2024-01-09 01:12:34 +0800

    Feat: update build script

--- script/build.sh
+++ script/build.sh
@@ -21,6 +21,7 @@ function Help() {
     echo "-v set build version (default: dev)"
     echo "-w init web version (default: build version)"
     echo "-s skip init web"
+    echo "-S set source dir (default: ../)"
     echo "-m set build mode (default: pie)"
     echo "-l set ldflags (default: -s -w --extldflags \"-static -fpic -Wl,-z,relro,-z,now\")"
     echo "-p set platform (default: host platform, support: all, linux, darwin, windows)"
@@ -42,14 +43,15 @@ function Init() {
         GIT_COMMIT="$commit"
     fi
     BUILD_MODE="pie"
-    LDFLAGS='-s -w --extldflags "-static -fpic -Wl,-z,relro,-z,now"'
+    LDFLAGS="-s -w --extldflags '-static -fpic -Wl,-z,relro,-z,now'"
     PLATFORM=""
     BUILD_DIR="../build"
     TAGS="jsoniter"
+    SOURCH_DIR="../"
 }
 
 function ParseArgs() {
-    while getopts "hsv:w:m:l:p:Pd:T:" arg; do
+    while getopts "hsS:v:w:m:l:p:Pd:T:" arg; do
         case $arg in
         h)
             Help
@@ -60,6 +62,9 @@ function ParseArgs() {
             ;;
         s)
             SKIP_INIT_WEB="true"
+            ;;
+        S)
+            SOURCH_DIR="$OPTARG"
             ;;
         w)
             WEB_VERSION="$OPTARG"
@@ -113,6 +118,11 @@ function FixArgs() {
 
     # trim / at the end
     BUILD_DIR="$(echo "$BUILD_DIR" | sed 's#/$##')"
+    if [ ! "$SOURCH_DIR" ] || [ ! -d "$SOURCH_DIR" ]; then
+        echo "source dir error: $SOURCH_DIR"
+        exit 1
+    fi
+    echo "build source dir: $SOURCH_DIR"
 }
 
 function InitDep() {
@@ -128,6 +138,52 @@ function InitDep() {
         exit 1
     fi
 }
+
+# https://go.dev/doc/install/source#environment
+# $GOOS	$GOARCH
+# aix	ppc64
+# android	386
+# android	amd64
+# android	arm
+# android	arm64
+# darwin	amd64
+# darwin	arm64
+# dragonfly	amd64
+# freebsd	386
+# freebsd	amd64
+# freebsd	arm
+# illumos	amd64
+# ios	arm64
+# js	wasm
+# linux	386
+# linux	amd64
+# linux	arm
+# linux	arm64
+# linux	loong64
+# linux	mips
+# linux	mipsle
+# linux	mips64
+# linux	mips64le
+# linux	ppc64
+# linux	ppc64le
+# linux	riscv64
+# linux	s390x
+# netbsd	386
+# netbsd	amd64
+# netbsd	arm
+# openbsd	386
+# openbsd	amd64
+# openbsd	arm
+# openbsd	arm64
+# plan9	386
+# plan9	amd64
+# plan9 arm
+# solaris	amd64
+# wasip1	wasm
+# windows	386
+# windows	amd64
+# windows	arm
+# windows   arm64
 
 # sqlite3 not support linux/loong64,linux/mips linux/mips64,linux/mips64le,linux/mipsle,linux/ppc64,
 LINUX_ALLOWED_PLATFORM="linux/386,linux/amd64,linux/arm,linux/arm64,linux/ppc64le,linux/riscv64,linux/s390x"
@@ -174,25 +230,76 @@ function Build() {
     echo "build $platform"
     GOOS=${platform%/*}
     GOARCH=${platform#*/}
+
     if [ "$GOOS" == "windows" ]; then
         EXT=".exe"
     else
         EXT=""
     fi
+
+    BUILD_FLAGS="-tags \"$TAGS\" -ldflags \"$LDFLAGS\""
+    BUILD_ENV="CGO_ENABLED=$CGO_ENABLED GOOS=$GOOS GOARCH=$GOARCH"
     if [ ! "$DISABLE_TRIM_PATH" ]; then
-        CGO_ENABLED=$CGO_ENABLED GOOS=$GOOS GOARCH=$GOARCH go build -trimpath -tags "$TAGS" -ldflags "$LDFLAGS" -o "$BUILD_DIR/$BIN_NAME-$GOOS-$GOARCH$EXT" ../
-    else
-        CGO_ENABLED=$CGO_ENABLED GOOS=$GOOS GOARCH=$GOARCH go build -tags "$TAGS" -ldflags "$LDFLAGS" -o "$BUILD_DIR/$BIN_NAME-$GOOS-$GOARCH$EXT" ../
+        BUILD_FLAGS="$BUILD_FLAGS -trimpath"
     fi
-    if [ $? -ne 0 ]; then
-        echo "build $GOOS/$GOARCH error"
-        exit 1
-    fi
+
+    # https://go.dev/doc/install/source#environment
+    case "$GOARCH" in
+    "386")
+        # default sse2
+        eval "$BUILD_ENV GO386=sse2 go build $BUILD_FLAGS -o \"$BUILD_DIR/$BIN_NAME-$GOOS-$GOARCH$EXT\" \"$SOURCH_DIR\""
+        echo "build $GOOS/$GOARCH softfloat"
+        eval "$BUILD_ENV GO386=softfloat go build $BUILD_FLAGS -o \"$BUILD_DIR/$BIN_NAME-$GOOS-$GOARCH-softfloat$EXT\" \"$SOURCH_DIR\""
+        ;;
+    "arm")
+        # default v7
+        # https://go.dev/wiki/GoArm
+        eval "$BUILD_ENV GOARM=7 go build $BUILD_FLAGS -o \"$BUILD_DIR/$BIN_NAME-$GOOS-$GOARCH$EXT\" \"$SOURCH_DIR\""
+        echo "build $GOOS/$GOARCH v5"
+        eval "$BUILD_ENV GOARM=5 go build $BUILD_FLAGS -o \"$BUILD_DIR/$BIN_NAME-$GOOS-$GOARCH-v5$EXT\" \"$SOURCH_DIR\""
+        echo "build $GOOS/$GOARCH v6"
+        eval "$BUILD_ENV GOARM=6 go build $BUILD_FLAGS -o \"$BUILD_DIR/$BIN_NAME-$GOOS-$GOARCH-v6$EXT\" \"$SOURCH_DIR\""
+        ;;
+    "amd64")
+        # default v1
+        # https://go.dev/wiki/MinimumRequirements#amd64
+        eval "$BUILD_ENV GOAMD64=v1 go build $BUILD_FLAGS -o \"$BUILD_DIR/$BIN_NAME-$GOOS-$GOARCH$EXT\" \"$SOURCH_DIR\""
+        echo "build $GOOS/$GOARCH v2"
+        eval "$BUILD_ENV GOAMD64=v2 go build $BUILD_FLAGS -o \"$BUILD_DIR/$BIN_NAME-$GOOS-$GOARCH-v2$EXT\" \"$SOURCH_DIR\""
+        echo "build $GOOS/$GOARCH v3"
+        eval "$BUILD_ENV GOAMD64=v3 go build $BUILD_FLAGS -o \"$BUILD_DIR/$BIN_NAME-$GOOS-$GOARCH-v3$EXT\" \"$SOURCH_DIR\""
+        echo "build $GOOS/$GOARCH v4"
+        eval "$BUILD_ENV GOAMD64=v4 go build $BUILD_FLAGS -o \"$BUILD_DIR/$BIN_NAME-$GOOS-$GOARCH-v4$EXT\" \"$SOURCH_DIR\""
+        ;;
+    "mips" | "mipsle" | "mips64" | "mips64le")
+        # default hardfloat
+        eval "$BUILD_ENV GOMIPS=hardfloat GOMIPS64=hardfloat go build $BUILD_FLAGS -o \"$BUILD_DIR/$BIN_NAME-$GOOS-$GOARCH$EXT\" \"$SOURCH_DIR\""
+        echo "build $GOOS/$GOARCH softfloat"
+        eval "$BUILD_ENV GOMIPS=softfloat GOMIPS64=softfloat go build $BUILD_FLAGS -o \"$BUILD_DIR/$BIN_NAME-$GOOS-$GOARCH-softfloat$EXT\" \"$SOURCH_DIR\""
+        ;;
+    "ppc64" | "ppc64le")
+        # default power8
+        eval "$BUILD_ENV GOPPC64=power8 go build $BUILD_FLAGS -o \"$BUILD_DIR/$BIN_NAME-$GOOS-$GOARCH$EXT\" \"$SOURCH_DIR\""
+        echo "build $GOOS/$GOARCH power9"
+        eval "$BUILD_ENV GOPPC64=power9 go build $BUILD_FLAGS -o \"$BUILD_DIR/$BIN_NAME-$GOOS-$GOARCH-power9$EXT\" \"$SOURCH_DIR\""
+        ;;
+    "wasm")
+        # no default
+        eval "$BUILD_ENV go build $BUILD_FLAGS -o \"$BUILD_DIR/$BIN_NAME-$GOOS-$GOARCH$EXT\" \"$SOURCH_DIR\""
+        echo "build $GOOS/$GOARCH satconv"
+        eval "$BUILD_ENV GOWASM=satconv go build $BUILD_FLAGS -o \"$BUILD_DIR/$BIN_NAME-$GOOS-$GOARCH-satconv$EXT\" \"$SOURCH_DIR\""
+        echo "build $GOOS/$GOARCH signext"
+        eval "$BUILD_ENV GOWASM=signext go build $BUILD_FLAGS -o \"$BUILD_DIR/$BIN_NAME-$GOOS-$GOARCH-signext$EXT\" \"$SOURCH_DIR\""
+        ;;
+    *)
+        eval "$BUILD_ENV go build $BUILD_FLAGS -o \"$BUILD_DIR/$BIN_NAME-$GOOS-$GOARCH$EXT\" \"$SOURCH_DIR\""
+        ;;
+    esac
 }
 
-function BuildSingle() {
-    GOOS="$(go env GOOS)"
-    GOARCH="$(go env GOARCH)"
+function BuildHost() {
+    GOOS="$(go env GOHOSTOS)"
+    GOARCH="$(go env GOHOSTARCH)"
     if [ "$GOOS" == "windows" ]; then
         EXT=".exe"
     else
@@ -200,9 +307,9 @@ function BuildSingle() {
     fi
     echo "build $GOOS/$GOARCH"
     if [ ! "$DISABLE_TRIM_PATH" ]; then
-        CGO_ENABLED=$CGO_ENABLED go build -trimpath -tags "$TAGS" -ldflags "$LDFLAGS" -o "$BUILD_DIR/$BIN_NAME$EXT" ../
+        CGO_ENABLED=$CGO_ENABLED GOOS=$GOOS GOARCH=$GOARCH go build -trimpath -tags "$TAGS" -ldflags "$LDFLAGS" -o "$BUILD_DIR/$BIN_NAME$EXT" "$SOURCH_DIR"
     else
-        CGO_ENABLED=$CGO_ENABLED go build -tags "$TAGS" -ldflags "$LDFLAGS" -o "$BUILD_DIR/$BIN_NAME$EXT" ../
+        CGO_ENABLED=$CGO_ENABLED GOOS=$GOOS GOARCH=$GOARCH go build -tags "$TAGS" -ldflags "$LDFLAGS" -o "$BUILD_DIR/$BIN_NAME$EXT" "$SOURCH_DIR"
     fi
     if [ $? -ne 0 ]; then
         echo "build $GOOS/$GOARCH error"
@@ -212,7 +319,7 @@ function BuildSingle() {
 
 function BuildAll() {
     if [ ! "$1" ]; then
-        BuildSingle
+        BuildHost
         return
     else
         for platform in $(echo "$1" | tr "," "\n"); do

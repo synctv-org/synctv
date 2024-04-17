bb1de8474d45e510e9a0212cb27ac13fcee8e8a3
zijiren233 <pyh1670605849@gmail.com>
2024-04-17 17:08:58 +0800

    Opt: add guest enabled setting to public settings

--- server/handlers/public.go
+++ server/handlers/public.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/synctv-org/synctv/internal/email"
+	"github.com/synctv-org/synctv/internal/settings"
 	"github.com/synctv-org/synctv/server/model"
 )
 
@@ -13,6 +14,8 @@ type publicSettings struct {
 	EmailDisableUserSignup bool     `json:"emailDisableUserSignup"`
 	EmailWhitelistEnabled  bool     `json:"emailWhitelistEnabled"`
 	EmailWhitelist         []string `json:"emailWhitelist,omitempty"`
+
+	GuestEnable bool `json:"guestEnable"`
 }
 
 func Settings(ctx *gin.Context) {
@@ -22,6 +25,8 @@ func Settings(ctx *gin.Context) {
 			EmailDisableUserSignup: email.DisableUserSignup.Get(),
 			EmailWhitelistEnabled:  email.EmailSignupWhiteListEnable.Get(),
 			EmailWhitelist:         strings.Split(email.EmailSignupWhiteList.Get(), ","),
+
+			GuestEnable: settings.EnableGuest.Get(),
 		},
 	))
 }

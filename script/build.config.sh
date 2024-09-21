414490e057653b5db8b31e1c2a6694db0719a294
zijiren233 <pyh1670605849@gmail.com>
2024-09-21 20:08:27 +0800

    revert: donot build ios

--- script/build.config.sh
+++ script/build.config.sh
@@ -43,7 +43,6 @@ function initDepPlatforms() {
 
     addAllowedPlatforms "linux/386,linux/amd64,linux/arm,linux/arm64,linux/loong64,linux/mips,linux/mips64,linux/mips64le,linux/mipsle,linux/ppc64le,linux/riscv64,linux/s390x"
     addAllowedPlatforms "darwin/amd64,darwin/arm64"
-    addAllowedPlatforms "ios/amd64,ios/arm64"
     addAllowedPlatforms "windows/386,windows/amd64,windows/arm64"
     addAllowedPlatforms "freebsd/386,freebsd/amd64,freebsd/arm,freebsd/arm64"
     addAllowedPlatforms "netbsd/amd64"

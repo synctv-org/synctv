18fd47f2132e39674406da06b1d5cf081387189e
zijiren233 <pyh1670605849@gmail.com>
2024-12-13 11:53:56 +0800

    feat: casdoor support

--- internal/provider/providers/casdoor.go
+++ internal/provider/providers/casdoor.go
@@ -12,45 +12,45 @@ import (
 	"golang.org/x/oauth2"
 )
 
-// https://openapi.logto.io/authentication
-type logtoProvider struct {
+// https://door.casdoor.com/.well-known/openid-configuration
+type casdoorProvider struct {
 	config   oauth2.Config
 	endpoint string
 }
 
-func newLogtoProvider() provider.Interface {
-	return &logtoProvider{
+func newCasdoorProvider() provider.Interface {
+	return &casdoorProvider{
 		config: oauth2.Config{
 			Scopes: []string{"profile", "email", "phone", "name", "openid"},
 		},
 	}
 }
 
-func (p *logtoProvider) Init(opt provider.Oauth2Option) {
+func (p *casdoorProvider) Init(opt provider.Oauth2Option) {
 	p.config.ClientID = opt.ClientID
 	p.config.ClientSecret = opt.ClientSecret
 	p.config.RedirectURL = opt.RedirectURL
 }
 
-func (p *logtoProvider) NewAuthURL(ctx context.Context, state string) (string, error) {
+func (p *casdoorProvider) NewAuthURL(ctx context.Context, state string) (string, error) {
 	return p.config.AuthCodeURL(state, oauth2.AccessTypeOnline), nil
 }
 
-func (p *logtoProvider) GetToken(ctx context.Context, code string) (*oauth2.Token, error) {
+func (p *casdoorProvider) GetToken(ctx context.Context, code string) (*oauth2.Token, error) {
 	return p.config.Exchange(ctx, code)
 }
 
-func (p *logtoProvider) RefreshToken(ctx context.Context, token string) (*oauth2.Token, error) {
+func (p *casdoorProvider) RefreshToken(ctx context.Context, token string) (*oauth2.Token, error) {
 	return p.config.TokenSource(ctx, &oauth2.Token{RefreshToken: token}).Token()
 }
 
-func (p *logtoProvider) GetUserInfo(ctx context.Context, code string) (*provider.UserInfo, error) {
+func (p *casdoorProvider) GetUserInfo(ctx context.Context, code string) (*provider.UserInfo, error) {
 	tk, err := p.GetToken(ctx, code)
 	if err != nil {
 		return nil, err
 	}
 	client := p.config.Client(ctx, tk)
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, p.endpoint+"/oidc/me", nil)
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, p.endpoint+"/api/userinfo", nil)
 	if err != nil {
 		return nil, err
 	}
@@ -59,12 +59,12 @@ func (p *logtoProvider) GetUserInfo(ctx context.Context, code string) (*provider
 		return nil, err
 	}
 	defer resp.Body.Close()
-	var ui logtoUserInfo
+	var ui casdoorUserInfo
 	err = json.NewDecoder(resp.Body).Decode(&ui)
 	if err != nil {
 		return nil, err
 	}
-	un := ui.Username
+	un := ui.PreferredUsername
 	if un == "" {
 		un = ui.Name
 	}
@@ -74,23 +74,22 @@ func (p *logtoProvider) GetUserInfo(ctx context.Context, code string) (*provider
 	}, nil
 }
 
-type logtoUserInfo struct {
-	Sub          string `json:"sub"`
-	Username     string `json:"username"`
-	PrimaryEmail string `json:"primaryEmail"`
-	PrimaryPhone string `json:"primaryPhone"`
-	Name         string `json:"name"`
-	Email        string `json:"email"`
+type casdoorUserInfo struct {
+	Sub               string `json:"sub"`
+	PreferredUsername string `json:"preferred_username"`
+	Name              string `json:"name"`
+	Email             string `json:"email"`
+	Phone             string `json:"phone"`
 }
 
-func (p *logtoProvider) RegistSetting(group string) {
+func (p *casdoorProvider) RegistSetting(group string) {
 	settings.NewStringSetting(
 		group+"_endpoint", "", group,
 		settings.WithAfterInitString(func(ss settings.StringSetting, s string) {
 			p.endpoint = s
 			p.config.Endpoint = oauth2.Endpoint{
-				AuthURL:  s + "/oidc/auth",
-				TokenURL: s + "/oidc/token",
+				AuthURL:  s + "/login/oauth/authorize",
+				TokenURL: s + "/api/login/oauth/access_token",
 			}
 		}),
 		settings.WithBeforeSetString(func(ss settings.StringSetting, s string) (string, error) {
@@ -103,17 +102,17 @@ func (p *logtoProvider) RegistSetting(group string) {
 		settings.WithAfterSetString(func(ss settings.StringSetting, s string) {
 			p.endpoint = s
 			p.config.Endpoint = oauth2.Endpoint{
-				AuthURL:  s + "/oidc/auth",
-				TokenURL: s + "/oidc/token",
+				AuthURL:  s + "/login/oauth/authorize",
+				TokenURL: s + "/api/login/oauth/access_token",
 			}
 		}),
 	)
 }
 
-func (p *logtoProvider) Provider() provider.OAuth2Provider {
-	return "logto"
+func (p *casdoorProvider) Provider() provider.OAuth2Provider {
+	return "casdoor"
 }
 
 func init() {
-	RegisterProvider(newLogtoProvider())
+	RegisterProvider(newCasdoorProvider())
 }

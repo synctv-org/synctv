a25dd2a283070d7e35af1dc9bbab4248b2d570f8
zijiren233 <84728412+zijiren233@users.noreply.github.com>
2026-07-29 16:13:48 +0000

    release: prepare v1.0.2-rc.4

--- docs/src/lib/project.ts
+++ docs/src/lib/project.ts
@@ -1,5 +1,5 @@
 const defaultRepository = 'synctv-org/synctv';
-const defaultAppVersion = '1.0.2-rc.3';
+const defaultAppVersion = '1.0.2-rc.4';
 
 function readEnv(name: string): string | undefined {
   const value = process.env[name]?.trim();
